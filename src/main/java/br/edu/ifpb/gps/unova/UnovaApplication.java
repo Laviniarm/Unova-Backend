caea4494a082a96715dff92b6e8ac09aada10ea5
Lavínia Rodrigues <96793999+Laviniarm@users.noreply.github.com>
2025-01-23 11:49:54 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/gps/unova/UnovaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.gps.unova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/gps/unova/UnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.gps.unova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
