3a0cb87e17fa1db31665783b04e7dc73d719e01d
Lavínia Rodrigues <96793999+Laviniarm@users.noreply.github.com>
2025-01-28 16:25:14 -0300

    Modelagem inicial dos cruds de produto e usuario

--- src/main/java/br/edu/ifpb/gps/unova/repository/ProductRepository.java
+++ src/main/java/br/edu/ifpb/gps/unova/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package br.edu.ifpb.gps.unova.repository;
 
-public class ProductRepository {
+import br.edu.ifpb.gps.unova.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/br/edu/ifpb/gps/unova/service/ProductService.java
+++ src/main/java/br/edu/ifpb/gps/unova/service/ProductService.java
@@ -1,4 +1,23 @@
 package br.edu.ifpb.gps.unova.service;
 
-public interface ProductService {
+import br.edu.ifpb.gps.unova.model.Product;
+import br.edu.ifpb.gps.unova.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
 }
