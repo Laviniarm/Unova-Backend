355a97d193517a4ff2475b5d560f5f802c9fa527
Lavínia Rodrigues <96793999+Laviniarm@users.noreply.github.com>
2025-03-07 16:05:18 -0300

    Crud carrinho de compras

---
+++ src/main/java/br/edu/ifpb/gps/unova/model/CartItem.java
@@ -0,0 +1,25 @@
+package br.edu.ifpb.gps.unova.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private String name;
+    private Double price;
+    private int quantity;
+}
