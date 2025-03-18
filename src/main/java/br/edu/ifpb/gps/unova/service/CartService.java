95c1b178c469f05b3f5c7d3b090a04937eab2b15
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2025-03-17 22:52:29 -0300

    Connecting cart back and front

--- src/main/java/br/edu/ifpb/gps/unova/service/CartService.java
+++ src/main/java/br/edu/ifpb/gps/unova/service/CartService.java
@@ -2,6 +2,9 @@ package br.edu.ifpb.gps.unova.service;
 
 import br.edu.ifpb.gps.unova.model.CartItem;
 import br.edu.ifpb.gps.unova.repository.CartItemRepository;
+import jakarta.transaction.Transactional;
+
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,11 +25,27 @@ public class CartService {
     }
 
     public CartItem addToCart(CartItem cartItem) {
+        if (cartItem.getId() != null) { // Se o ID não for nulo, verificamos se o item já existe no banco
+            Optional<CartItem> existingItem = cartItemRepository.findById(cartItem.getId());
+            if (existingItem.isPresent()) {
+                CartItem managedItem = existingItem.get();
+                managedItem.setQuantity(managedItem.getQuantity() + cartItem.getQuantity());
+                return cartItemRepository.save(managedItem); // Salvar o item já gerenciado
+            }
+        }
+    
+        cartItem.setVersion(0); // Se for um novo item, garantir que a versão seja inicializada corretamente
         return cartItemRepository.save(cartItem);
+        
     }
 
     public void removeFromCart(Long id) {
         cartItemRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateCartItem(Long id, Integer quantity) {
+        cartItemRepository.updateQuantity(id, quantity);
     }
 
     public void clearCart() {
